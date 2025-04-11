5fc23002f79ad40466d84becb4b27e4638e76fa4
patryk4815 <patryk.sondej@grupawp.pl>
2025-04-12 00:25:26 +0200

    fix pwndbg-lldb broken release

--- Casks/pwndbg-lldb.rb
+++ Casks/pwndbg-lldb.rb
@@ -2,7 +2,7 @@ cask "pwndbg-lldb" do
   arch arm: "arm64", intel: "amd64"
 
   version "2025.02.19"
-  sha256 arm:   "ad2ec2d1711de4b309ee07fd8f0b14ad7ea67b1cffad53e8fc5d3bdd515b7307",
+  sha256 arm:   "c95e5aab29197c751720d7b5f9ede285485f2d5aa5b8335052e3f1c08c280a5e",
          intel: "b84a8c0d576fc88dea6fede0ab3a870b93c740f1921f67ff72f14f149d3c1a3a"
 
   url "https://github.com/pwndbg/pwndbg/releases/download/#{version}/pwndbg-lldb_#{version}_macos_#{arch}-portable.tar.xz"
