b7a96b747e78800f00ef956d51ed1af69d5c9c72
patryk4815 <patryk.sondej@grupawp.pl>
2025-04-17 04:30:51 +0200

    pwndbg-gdb bump 2025.04.13

--- Casks/pwndbg-gdb.rb
+++ Casks/pwndbg-gdb.rb
@@ -1,9 +1,9 @@
 cask "pwndbg-gdb" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2025.02.19"
-  sha256 arm:   "29b6e65affb6b0c22eaefc29fbe1d96afc0b667fce440af4bf363aaf50c8e727",
-         intel: "83eb5444a4217c57d4add829f94a17e016f55b76027d905f8be13fc46f93e6ad"
+  version "2025.04.13"
+  sha256 arm:   "7478e165333f7e1dc85edaddbcbf2438fc848ffd063431fd2430cd8a2b74996c",
+         intel: "d55b22d442c937262e3b49fc45976ee0cb3548dd105f9a46339b3c430ef245c7"
 
   url "https://github.com/pwndbg/pwndbg/releases/download/#{version}/pwndbg_#{version}_macos_#{arch}-portable.tar.xz"
   name "pwndbg-gdb"
