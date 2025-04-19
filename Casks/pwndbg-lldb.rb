853fa8f4eb3f334b7b69ffdf8c7bdc31a39ce811
patryk4815 <patryk.sondej@grupawp.pl>
2025-04-19 14:34:25 +0200

    bump 2025.04.18

--- Casks/pwndbg-lldb.rb
+++ Casks/pwndbg-lldb.rb
@@ -1,7 +1,7 @@
 cask "pwndbg-lldb" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2025.04.13"
+  version "2025.04.18"
   sha256 arm:   "6e5ec6933031a1d5b4fcb4dbd215114f555a0d7645ebcf346fdea57713e59c23",
          intel: "75f488824d91462bd78ff7881f2c97e7ae4ffd397691bd76d3cc1963a3365a7d"
 
