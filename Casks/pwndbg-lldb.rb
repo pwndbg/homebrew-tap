10c13f641f4c5b0539d92bcb181cc9d8de327fcb
patryk4815 <patryk.sondej@grupawp.pl>
2026-02-17 23:14:11 +0100

    Change release link

--- Casks/pwndbg-lldb.rb
+++ Casks/pwndbg-lldb.rb
@@ -5,7 +5,7 @@ cask "pwndbg-lldb" do
   sha256 arm:   "4b0fd513c4dd9ba1d34780b061fc42f6ff7c370f9fa64417b724660bb9fbb466",
          intel: "e762c3c4dbaa61703d2ddb8f952d4e0bed0fbe403168011b43e5e42fdfa8e19f"
 
-  url "https://github.com/pwndbg/pwndbg/releases/download/#{version}/pwndbg-lldb_#{version}_macos_#{arch}-portable.tar.xz"
+  url "https://releases.pwndbg.re/releases/#{version}/pwndbg-lldb_#{version}_macos_#{arch}-portable.tar.xz"
   name "pwndbg-lldb"
   desc "Exploit Development and Reverse Engineering with LLDB Made Easy"
   homepage "https://github.com/pwndbg/pwndbg"
